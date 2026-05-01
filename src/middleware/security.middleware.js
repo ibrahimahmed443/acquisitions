2b48010ad90d0b5e50b002dfd981073c387c7c12
Ibrahim Ahmed <ibrahimahmed443@gmail.com>
2026-05-01 18:48:21 +0500

    prettier

--- src/middleware/security.middleware.js
+++ src/middleware/security.middleware.js
@@ -40,12 +40,10 @@ const securityMiddleware = async (req, res, next) => {
         path: req.path,
       });
 
-      return res
-        .status(403)
-        .json({
-          error: 'Forbidden',
-          message: 'Automated requests are not allowed',
-        });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Automated requests are not allowed',
+      });
     }
 
     if (decision.isDenied() && decision.reason.isShield()) {
@@ -56,12 +54,10 @@ const securityMiddleware = async (req, res, next) => {
         method: req.method,
       });
 
-      return res
-        .status(403)
-        .json({
-          error: 'Forbidden',
-          message: 'Request blocked by security policy',
-        });
+      return res.status(403).json({
+        error: 'Forbidden',
+        message: 'Request blocked by security policy',
+      });
     }
 
     if (decision.isDenied() && decision.reason.isRateLimit()) {
@@ -78,12 +74,10 @@ const securityMiddleware = async (req, res, next) => {
     next();
   } catch (error) {
     console.log('Arcjet middleware error:', error);
-    res
-      .status(500)
-      .json({
-        error: 'Internal Server Error',
-        message: 'Something went wrong with security middleware middleware',
-      });
+    res.status(500).json({
+      error: 'Internal Server Error',
+      message: 'Something went wrong with security middleware middleware',
+    });
   }
 };
 
